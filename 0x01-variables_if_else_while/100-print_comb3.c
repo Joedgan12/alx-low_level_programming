b1f940a2818e332f54433968168c6315863369ac
Joedgan12 <joepackins2gmail.com>
2023-10-06 15:13:36 -0700

    100-print_comb3

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+/**
+ * main - function to print combination of two numbers
+ *
+ * Return; always 0
+ */
+
+int main(void)
+{
+	int m = '0';
+	int n = '0';
+
+	while (m <= '9')
+	{
+		while (n <= '9')
+		{
+			if (!(m > n) || m == n)
+			{
+				putchar(m);
+				putchar(n);
+				if (m == '8' && n == '9')
+				{
+					putchar('\n');
+				}
+				else
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+			n++;
+		}
+		n = '0';
+		m++;
+
+	}
+return (0);
+}
