0214bb45d87740402cd80979bf9febe58ce4a1cc
Joedgan12 <joepackins@gmail.com>
2023-10-11 14:35:56 -0700

    islower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -7,12 +7,13 @@
  */
 
 int _islower(int c)
+
 {
 
-        if (c>= 97 && c <= 122)
-        {
+	if (c >= 97 && c <= 122)
+	{
 
-                return (1);
-        }
-        return (0);
+		return (1);
+	}
+	return (0);
 }
