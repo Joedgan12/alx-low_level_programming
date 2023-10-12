8b73a3feeeeee4588e89696faacb5a4689847b31
Joedgan12 <joepackins@gmail.com>
2023-10-12 13:36:00 -0700

    100 main

--- 0x02-functions_nested_loops/100-main.c
+++ 0x02-functions_nested_loops/100-main.c
@@ -1,3 +1,4 @@
+
 #include "main.h"
 
 /**
@@ -7,12 +8,12 @@
  */
 int main(void)
 {
-    print_times_table(3);
-    _putchar('\n');
-    print_times_table(5);
-    _putchar('\n');
-    print_times_table(98);
-    _putchar('\n');
-    print_times_table(12);  
-    return (0);
+print_times_table(3);
+_putchar('\n');
+print_times_table(5);
+_putchar('\n');
+print_times_table(98);
+_putchar('\n');
+prin_times_table(12);  
+return (0);
 }
