b600d2c848d57419cb455c1df0110beb7976c760
Joedgan12 <joepackins@gmail.com>
2023-10-12 13:10:52 -0700

    natural

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,21 +1,19 @@
-#include "main.h"
-
-
+#include <stdio.h>
 /**
- * main - check the code.
- *
- * Return: Always 0.
+ * main - function to summultiples of 3 or 5 in numbers below 1024
+ * Return: 0
  */
-
 
 int main(void)
 {
-	print_times_table(3);
-	_putchar('\n');
-	print_times_table(5);
-	_putchar('\n');
-	print_times_table(98);
-	_putchar('\n');
-	print_times_table(12);
+	int sum = 0;
+	int i;
+
+	for (i = 0 ; i < 1024 ; i++)
+	{
+		if (i % 3 == 0 || i % 5 == 0)
+			sum = sum + 1;
+	}
+	printf("%d\n", sum);
 	return (0);
 }
