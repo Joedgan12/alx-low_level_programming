8c65e989f256daaafd6fa243b93fc84e3324300e
Joedgan12 <joepackins@gmail.com>
2023-10-19 15:00:56 -0700

    4-rev_array

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+  * reverse_array - The main faunction
+  *
+  * @a: Function parameter
+  *
+  * @n: Function parameter
+  *
+  * Return: Always 0
+  */
+void reverse_array(int *a, int n)
+{
+	int k, r;
+
+	for (k = 0; k < n--; k++)
+	{
+	r = a[k];
+	a[k] = a[n];
+	a[n] = r;
+	}
+}
+
