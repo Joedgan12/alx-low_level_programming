71d495aad88629c3426ff75e68fec0d2dd472115
Joedgan12 <joepackins@gmail.com>
2023-11-01 14:02:56 -0700

    5-rev_string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -18,7 +18,7 @@ void rev_string(char *s)
 	for (index = len - 1; index >= len / 2; index--)
 	{
 	tmp = s[index];
-	s[index] = s[len - index -1];
+	s[index] = s[len - index - 1];
 	s[len - index - 1] = tmp;
 	}
 }	
