21445117266cf2e9f13153104a4d03401b641688
Joedgan12 <joepackins@gmail.com>
2023-11-01 17:54:39 -0700

    100-change.c

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * print the minimum number of coin to make a change for an amount
+ * main - print the minimum number of coin to make a change for an amount
  * @argc: number of command line arg
  * @argv: arrays that ho;ds the command line arg...
  * Return: (0)
