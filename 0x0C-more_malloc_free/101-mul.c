38bafb327adbbdcadf09a45ed843999323e13b23
Joedgan12 <joepackins@gmail.com>
2023-11-03 14:23:06 -0700

    101-mul

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -57,6 +57,7 @@ int main(int argc, char *argv[])
 {
 	char *s1, *s2;
 	int len1, len2, len, i, carry, digit1, digit2, *result, a = 0;
+
 	s1 = argv[1], s2 = argv[2];
 	if (argc != 3 || !is_digit(s1) || !is_digit(s2))
 		errors();
@@ -85,9 +86,9 @@ int main(int argc, char *argv[])
 	for (i = 0; i < len - 1; i++)
 	{
 		if (result[i])
-				a = 1;
-			if (a)
-				_putchar(result[i] + '0');
+			a = 1;
+		if (a)
+			_putchar(result[i] + '0');
 	}
 	if (!a)
 	_putchar('0');
