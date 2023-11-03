26e25b3fa69fdee17d97e023de8d605adea5e0db
Joedgan12 <joepackins@gmail.com>
2023-11-03 05:02:09 -0700

    0-malloc_checked.c

--- 0x0C-more_malloc_free/0-main.c
+++ 0x0C-more_malloc_free/0-main.c
@@ -14,7 +14,7 @@ int main(void)
 	int *i;
 	float *f;
 	double *d;
-	
+
 	c = malloc_checked(sizeof(char) * 1024);
 	printf("%p\n", (void *)c);
 	i = malloc_checked(sizeof(int) * 402);
--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -17,6 +17,6 @@ void *malloc_checked(unsigned int b)
 
 	if (p == NULL)
 		exit(98);
-	
+
 	return (p);
 }
