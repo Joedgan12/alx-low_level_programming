d8c6720f916fe1ed364a5cf3ba60051e97c02a76
Joedgan12 <joepackins@gmail.com>
2023-11-09 14:46:03 -0800

    2-print_strings.c

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -4,7 +4,6 @@
 #include <stdio.h>
 #include <stdarg.h>
 
-_putchar(char);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
